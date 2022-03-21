a1d4d6e77043a88f4d92b0639b116af9f92be3e5
Hans Liao <hans.liao@tomtom.com>
2022-03-21 15:25:23 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='fancy_logger',
+      version='0.1',
+      description='Fancy logger with colour support',
+      url='https://github.com/poseidon0206/fancy_logger',
+      author='Hans Liao',
+      author_email='poseidon206@gmail.com',
+      license='MIT',
+      packages=['fancy_logger'],
+      zip_safe=False
+)
